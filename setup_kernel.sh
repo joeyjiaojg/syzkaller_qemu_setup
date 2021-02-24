64e54ae3bae279e31f02de72ed9adc32845e6323
Joey Jiao <joeyjiaojg@qq.com>
2021-02-24 17:44:01 +0800

    Fix

--- setup_kernel.sh
+++ setup_kernel.sh
@@ -3,6 +3,6 @@ epxort KERNEL=kernel-git
 git clone https://github.com/torvalds/linux.git $KERNEL
 cd $KERNEL
 make defconfig
-echo -e "CONFIG_KCOV=y\nCONFIG_DEBUG_INFO=y\nCONFIG_KASAN=y\nCONFIG_KASAN_INLINE=y\nCONFIG_DEBUG_INFO=y\nCONFIG_CONFIG_FS=y\nCONFIG_SECURITYFS=y" >> .config
+echo -e "CONFIG_KCOV=y\nCONFIG_DEBUG_INFO=y\nCONFIG_KASAN=y\nCONFIG_KASAN_INLINE=y\nCONFIG_DEBUG_INFO=y\nCONFIG_CONFIGFS_FS=y\nCONFIG_SECURITYFS=y" >> .config
 yes "" | make oldconfig
 make -j$(nproc)
